797b92580c6d5743c8237aa17a180aa1074ee1e9
John Knapp <john.knappster@gmail.com>
2025-09-13 18:06:10 -0700

    feat(frontend): scaffold Vite React TS app with sessions/updates view and Jest setup

--- action-frontend/jest.config.cjs
+++ action-frontend/jest.config.cjs
@@ -1,11 +1,12 @@
 module.exports = {
   testEnvironment: 'jsdom',
   transform: {
-    '^.+\\.(ts|tsx)$': ['ts-jest', { tsconfig: 'tsconfig.json' }],
+    '^.+\.(ts|tsx)$': ['ts-jest', { tsconfig: 'tsconfig.json' }],
   },
   moduleFileExtensions: ['ts', 'tsx', 'js'],
   extensionsToTreatAsEsm: ['.ts', '.tsx'],
   transformIgnorePatterns: ['/node_modules/'],
+  setupFiles: ['whatwg-fetch']
 };
 
 
